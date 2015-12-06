1139ba613af98b3bb535778611d5897d5e16783f
Dell Donatoni <ddonatoni@elevatestudios.com>
2015-12-05 19:55:09 -0600

    Instgram Chrome Extension base setup.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var nes;
-$(function() {
-    nes = new JSNES({
-        'ui': $('#emulator').JSNESUI({
-            "Roms": [
-            	['Super Mario Bros', 'roms/Super Mario Bros.nes'],
-            	['Super Mario Bros 2', 'roms/Super Mario Bros 2.nes'],
-                ['Super Mario Bros 3', 'roms/Super Mario Bros 3.nes'],
-                ['Legend of Zelda', 'roms/Legend of Zelda.nes'],
-                ['Zelda 2 - The Adventure of Link', 'roms/Zelda 2 - The Adventure of Link.nes'],
-                ['Kung Fu', 'roms/Kung Fu.nes']
-            ]
-        })
+$(document).ready(function() {
+    $('.my-instashow').instaShow({
+        accessToken: '47389409.745c638.11ecb4d9ac2f4a63ad1ff32d3ab2e34f',
+        source: '@delatoni',
+        width: '100%',
+        height: '100%',
+        direction: 'vertical',
+        //columns: '6',
+        //rows: '6',
+        arrowsControl: 'false',
+        scrollControl: 'true',
+        lang: 'en',
+        popupInfo: 'username, instagramLink, likesCounter, commentsCounter, location, passedTime, description, comments'
     });
 });
